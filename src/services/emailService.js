dcd78ca74fc59e5674971ef55faf897d1eaae9dd
baticlean <baticlean225@gmail.com>
2026-05-12 03:04:01 +0000

    V1.1

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,19 +1,19 @@
-const Brevo = require('@getbrevo/brevo');
+const { TransactionalEmailsApi, SendSmtpEmail, TransactionalEmailsApiApiKeys } = require('@getbrevo/brevo');
 
 /**
  * Pourquoi : Isoler l'envoi d'emails dans un service dédié permet de changer 
  * de fournisseur facilement. On utilise @getbrevo/brevo car sib-api-v3-sdk est déprécié.
  */
 
-const { generateTicketTemplate } = require('../utils/emailTemplate');
+const { generateTicketTemplate, generateEventUpdateTemplate, generateEventCancellationTemplate } = require('../utils/emailTemplate');
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
+const apiInstance = new TransactionalEmailsApi();
+apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
 
 const sendTicketEmail = async (attendee) => {
   const { email, nom, prenoms, uuid } = attendee;
 
-  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+  const sendSmtpEmail = new SendSmtpEmail();
 
   sendSmtpEmail.subject = "MeetNova - Votre billet d'entrée est arrivé !";
   sendSmtpEmail.to = [{ email, name: `${prenoms} ${nom}` }];
@@ -34,7 +34,7 @@ const sendTicketEmail = async (attendee) => {
 };
 
 const sendEventUpdateEmail = async (email, prenoms, eventTitle, eventDetails) => {
-  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+  const sendSmtpEmail = new SendSmtpEmail();
   sendSmtpEmail.subject = `Mise à jour : ${eventTitle}`;
   sendSmtpEmail.to = [{ email, name: prenoms }];
   sendSmtpEmail.sender = { email: process.env.BREVO_SENDER_EMAIL, name: process.env.BREVO_SENDER_NAME };
@@ -48,7 +48,7 @@ const sendEventUpdateEmail = async (email, prenoms, eventTitle, eventDetails) =>
 };
 
 const sendEventCancellationEmail = async (email, prenoms, eventTitle) => {
-  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+  const sendSmtpEmail = new SendSmtpEmail();
   sendSmtpEmail.subject = `Annulation : ${eventTitle}`;
   sendSmtpEmail.to = [{ email, name: prenoms }];
   sendSmtpEmail.sender = { email: process.env.BREVO_SENDER_EMAIL, name: process.env.BREVO_SENDER_NAME };
